2cafc9666ff868b15ed5c9d3ed5a589a84b2bffb
Madhuvanthi Srivatsav R <131163302+madhusrivatsav@users.noreply.github.com>
2025-11-19 16:04:38 +0530

    Update resnet_9.py

--- network_arch/resnet_9.py
+++ network_arch/resnet_9.py
@@ -166,6 +166,151 @@ class ResNet9_100(nn.Module):
            out = self.classifier(out) 
         return out   
 
+def conv_block_Kr2(out_channels, pool=False):
+    layers = [nn.BatchNorm2d(out_channels),
+              nn.ReLU(inplace=True)]
+    if pool: layers.append(nn.MaxPool2d(2))
+    return nn.Sequential(*layers)
+
+class ResNet9_100_temp1(nn.Module):
+    def __init__(self, in_channels, out=100,gamma=0,temp0 = False, temp1=False,temp2=False,temp3=False,temp4=False,temp5=False,temp6=False,temp7=False,temp8=False,sparse=False,drop=1):
+        super().__init__()
+        self.temp0 = temp0
+        self.temp1 = temp1
+        self.temp2 = temp2
+        self.temp3 = temp3
+        self.temp4 = temp4
+        self.temp5 = temp5
+        self.temp6 = temp6
+        self.temp7 = temp7
+        self.temp8 = temp8
+        
+        if(temp0):
+            self.c1 = temp_conv_k_opt(in_channels=in_channels, out_channels= 16*4, gamma=gamma[0],diff=0, sparse=sparse,drop_prob=drop)
+            self.conv1 = conv_block_Kr2(16*4)
+        else:
+            print("1")
+            self.conv1 = conv_block(in_channels, 16*4)
+            
+        if(temp1):
+            self.c2 = temp_conv_k_opt(16*4, 32*4, kernel_size=3,gamma=gamma[1],diff=0,sparse=sparse,drop_prob = drop)#64*9
+            self.conv2 = conv_block_Kr2(32*4, pool=True)
+        else:
+            print("2")
+            self.conv2 = conv_block(16*4, 32*4, pool=True)
+            
+        if(temp2):
+            self.res11 = temp_conv_k_opt(32*4, 32*4,kernel_size=3,gamma=gamma[2],diff=0,sparse=sparse,drop_prob = drop)
+            self.res12 = conv_block_Kr2(32*4)
+        else:
+            print("3")
+            self.res11 = (conv_block(32*4, 32*4))
+        
+        if(temp3):
+            self.res21 = temp_conv_k_opt(32*4, 32*4,kernel_size=3,gamma=gamma[3],diff=0,sparse=sparse,drop_prob = drop)
+            self.res22 = conv_block_Kr2(32*4)
+        else:
+            print("4")
+            self.res22 = conv_block(32*4, 32*4)
+        
+        if(temp4):    
+            self.c3 = temp_conv_k_opt(32*4, 64*4, kernel_size=3,gamma=gamma[4],diff=0,sparse=sparse,drop_prob = drop)#64*9
+            self.conv3 = conv_block_Kr2(64*4, pool=True)
+        else:
+            print("5")
+            self.conv3 = conv_block(32*4, 64*4, pool=True)
+        
+        if(temp5):
+            self.c4 = temp_conv_k_opt(64*4, 128*4, kernel_size=3,gamma=gamma[5],diff=0,sparse=sparse,drop_prob = drop)#64*9
+            self.conv4 = conv_block_Kr2(128*4, pool=True)
+        else:
+            print("6")
+            self.conv4 = conv_block(64*4, 128*4, pool=True)
+        
+        if(temp6):
+            self.res211 = temp_conv_k_opt(128*4, 128*4,kernel_size=3,gamma=gamma[6],diff=0,sparse=sparse,drop_prob = drop)
+            self.res212 = conv_block_Kr2(128*4)
+        else:
+            print("7")
+            self.res211 = (conv_block(128*4, 128*4))
+        
+        if(temp7):
+            self.res221 = temp_conv_k_opt(128*4, 128*4,kernel_size=3,gamma=gamma[7],diff=0,sparse=sparse,drop_prob = drop)
+            self.res222 = conv_block_Kr2(128*4)
+        else:
+            print("8")
+            self.res222 = conv_block(128*4, 128*4)
+      
+        if(temp8):
+            self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        MPLayer_in_K(128*4, out,gamma = gamma[8],diff=0,sparse=sparse,drop_prob = drop))
+        else:
+            print("9")
+            self.classifier = nn.Sequential(nn.MaxPool2d(4), 
+                                        nn.Flatten(), 
+                                        nn.Linear(128*4, out,bias=False))
+
+        self.d = nn.Dropout2d(0.1)
+        # self.bn = nn.BatchNorm1d(10,affine=False)
+        
+    def forward(self, xb):
+        
+        if(self.temp0):
+            out = -1*self.c1(xb)
+            out = self.conv1(out)
+        else:
+            out = self.conv1(xb)
+
+        if(self.temp1):
+            out= -1*self.c2(out) #64*9 = 576
+            out = self.conv2(out)
+        else:
+            out = self.conv2(out)
+            
+        if(self.temp2):
+            out1 = -1*self.res11(out)
+            out1 = self.res12(out1)
+        else:
+            out1 = self.res11(out)
+        
+        if(self.temp3):
+            out1 = -1*self.res21(out1)
+            out = self.res22(out1) + out
+        else:
+            out = self.res22(out1) + out
+                
+        # out = self.d(out)
+        if(self.temp4):
+            out = -1*self.c3(out) #64*9 = 576
+            out = self.conv3(out)
+        else:
+            out = self.conv3(out)
+        
+        if(self.temp5):
+            out = -1*self.c4(out) #64*9 = 576
+            out = self.conv4(out)
+        else:
+            out = self.conv4(out)
+            
+        if(self.temp6):
+            out1 = -1*self.res211(out)
+            out1 = self.res212(out1)
+        else:
+            out1 = self.res211(out)
+        
+        if(self.temp7):
+            out1 = -1*self.res221(out1)
+            out = self.res222(out1) + out
+        else:
+            out = self.res222(out1) + out
+            
+        if(self.temp8):
+            out = -10*self.classifier(out)
+        else:
+           out = self.classifier(out) 
+        return out   
+        
 #pi^K model with noise, pkt_drop
 class ResNet9_100_temp_noise(nn.Module):
     def __init__(self, in_channels, out=100,gamma=0,std = 0,temp0 = False, temp1=False,temp2=False,temp3=False,temp4=False,temp5=False,temp6=False,temp7=False,temp8=False,sparse=False,drop=1):
